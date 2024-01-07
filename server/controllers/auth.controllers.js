ef31221ad247b06cfcdeee4480a6471eb26462ef
visaljudan <visal.nls2003@gmail.com>
2024-01-07 12:15:53 +0700

    Test api

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,4 +1,14 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 //Sign Up
 export const signup = async (req, res, next) => {
-  res.status(201).json("User created successfully!");
+  const { nameuser, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ nameuser, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
