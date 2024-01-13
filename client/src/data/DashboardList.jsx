8fa05277e856dd103b05f4aac08736ca1c2e89c7
visaljudan <visal.nls2003@gmail.com>
2024-01-13 14:04:43 +0700

    update

--- client/src/data/DashboardList.jsx
+++ client/src/data/DashboardList.jsx
@@ -6,33 +6,8 @@ const DashboardList = [
   },
   {
     id: 2,
-    href: "#analytics",
-    value: "Analytics",
-  },
-  {
-    id: 3,
     href: "/admin/dashboard/confirm",
     value: "Report",
-  },
-  {
-    id: 4,
-    href: "#setting",
-    value: "Setting",
-  },
-  {
-    id: 5,
-    href: "#message",
-    value: "Message",
-  },
-  {
-    id: 6,
-    href: "#chart",
-    value: "Chart",
-  },
-  {
-    id: 7,
-    href: "#resetpasswrod",
-    value: "Reset Passwrod",
   },
 ];
 export default DashboardList;
